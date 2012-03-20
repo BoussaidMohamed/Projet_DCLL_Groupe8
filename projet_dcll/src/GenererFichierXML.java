a26c9a861700ea80f18a7a87279a9d300e1f6231
Stephanie GAUTIER <stephanie.gautier1@gmail.com>
2012-03-20 20:46:12 +0100

    utilisation du plugin checkstyle, vérifier que la classe respecte bien les normes de checkstyles
    ajout d'un fichier xml généré par GenererFichierXML

--- projet_dcll/src/GenererFichierXML.java
+++ projet_dcll/src/GenererFichierXML.java
@@ -2,54 +2,99 @@
 import java.io.*;
 import org.jdom.*;
 import org.jdom.output.*;
-
+/**
+ *@author GAUTIER STéphanie
+ *This class permits to generate a xml file.
+ */
 public class GenererFichierXML {
-   static Element racine = new Element("quiz");
+	/**
+	 * element racine.
+	 */
+	private static Element racine = new Element("quiz");
+	/**
+	 * document.
+	 */
+	private static org.jdom.Document document = new Document(racine);
+	/**
+	 * @return racine
+	 */
+   public static Element getRacine() {
+		return racine;
+	}
 
-   static org.jdom.Document document = new Document(racine);
-   
-   static void affiche()
-   {
-      try
-      {
+   /**
+    * @param racine
+    * @see setRacine#racine
+    */
+	public static void setRacine(final Element racine) {
+		GenererFichierXML.racine = racine;
+	}
+
+	/**
+	 * @return document
+	 */
+	public static org.jdom.Document getDocument() {
+		return document;
+	}
+
+	/**
+	 * @param document
+	 * @see serDocument#document
+	 */
+	public static void setDocument(final org.jdom.Document document) {
+		GenererFichierXML.document = document;
+	}
+
+
+	/**
+	 */
+	static void affiche() {
+      try {
          //On utilise ici un affichage classique avec getPrettyFormat()
          XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
          sortie.output(document, System.out);
-      }
-      catch (java.io.IOException e){}
+      } catch (java.io.IOException e) {
+    	  System.out.println("Erreur de format");
+      	}
    }
 
-   static void enregistre(String fichier)
-   {
-      try
-      {
+	/**
+	 * @param fichier
+	 */
+	static void enregistre(final String fichier) {
+      try {
          //On utilise ici un affichage classique avec getPrettyFormat()
          XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
-         //Remarquez qu'il suffit simplement de créer une instance de FileOutputStream
+         //Remarquez qu'il suffit simplement de créer une instance,
+         //de FileOutputStream
          //avec en argument le nom du fichier pour effectuer la sérialisation.
          sortie.output(document, new FileOutputStream(fichier));
-      }
-      catch (java.io.IOException e){}
+      } catch (java.io.IOException e) {
+    	  System.out.println("Erreur de format");
+      	}
    }
 
-   public static void main(String[] args) {
-      
+	/**
+	 * @param args.
+	 */
+   public final void genererFichierXML() {
+	   String phrase = "Tomcat est un conteneur implementant";
+	   phrase += "toutes les specifications JEE";
       Element question = new Element("question");
       racine.addContent(question);
-      Attribute type = new Attribute("type","truefalse");
+      Attribute type = new Attribute("type", "truefalse");
       question.setAttribute(type);
-      
       Element name = new Element("name");
       question.addContent(name);
       Element text1 = new Element("text");
       text1.setText("Tomcat et JEE");
       name.addContent(text1);
       Element questiontext = new Element("questiontext");
-      Attribute type2 = new Attribute("format","moodle_format");
+      Attribute type2 = new Attribute("format", "moodle_format");
       questiontext.setAttribute(type2);
       question.addContent(questiontext);
       Element text3 = new Element("text");
-      text3.setText("Tomcat est un conteneur implementant toutes les specifications JEE.");
+      text3.setText(phrase);
       questiontext.addContent(text3);
       Element image = new Element("image");
       question.addContent(image);
@@ -70,7 +115,7 @@ public class GenererFichierXML {
       shuffleansnwers.setText("0");
       question.addContent(shuffleansnwers);
       Element answer1 = new Element("answer");
-      Attribute type3 = new Attribute("fraction","0");
+      Attribute type3 = new Attribute("fraction", "0");
       answer1.setAttribute(type3);
       question.addContent(answer1);
       Element text4 = new Element("text");
@@ -82,7 +127,7 @@ public class GenererFichierXML {
       text5.setText("Tomcat est un conteneur Web uniquement");
       feedback.addContent(text5);
       Element answer2 = new Element("answer");
-      Attribute type4 = new Attribute("fraction","100");
+      Attribute type4 = new Attribute("fraction", "100");
       answer2.setAttribute(type4);
       question.addContent(answer2);
       Element text6 = new Element("text");
@@ -93,8 +138,6 @@ public class GenererFichierXML {
       Element text7 = new Element("text");
       text7.setText("Tomcat est un conteneur Web uniquement");
       feedback2.addContent(text7);
-
-      
       affiche();
       enregistre("Exercice1.xml");
    }
